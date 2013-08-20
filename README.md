56e9ac1b39945170be8b9b44dffd8350a7a0d6ce
Kannan Manickam <kannan@rightscale.com>
2013-08-19 23:46:14 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# synergy cookbook
+This is a work-in-progress cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Kannan Manickam (<me@arangamani.net>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'synergy'
+maintainer       'Kannan Manickam'
+maintainer_email 'me@arangamani.net'
+license          'Apache 2.0'
+description      'Installs/Configures synergy'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
+
